8f94c9cac2e41a331207c13ed56fcc5fe8ea1ed6
sazzadhossain1 <sazzadtomal1@gmail.com>
2023-04-26 01:40:28 +0600

    show some information in react boostrap cart from home page

--- src/Pages/Shared/NewsSummeryCart/NewsSummeryCart.js
+++ src/Pages/Shared/NewsSummeryCart/NewsSummeryCart.js
@@ -1,7 +1,19 @@
 import React from "react";
+import Card from "react-bootstrap/Card";
 
 const NewsSummeryCart = ({ news }) => {
-  return <div>{news.title}</div>;
+  return (
+    <Card className="text-center">
+      <Card.Header>Featured</Card.Header>
+      <Card.Body>
+        <Card.Title>Special title treatment</Card.Title>
+        <Card.Text>
+          With supporting text below as a natural lead-in to additional content.
+        </Card.Text>
+      </Card.Body>
+      <Card.Footer className="text-muted">2 days ago</Card.Footer>
+    </Card>
+  );
 };
 
 export default NewsSummeryCart;
