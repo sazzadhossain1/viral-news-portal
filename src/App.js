c12966c64739c11c20430a0e37b8b4df3b82349e
sazzadhossain1 <sazzadtomal1@gmail.com>
2023-04-26 08:22:26 +0600

    create auth context and set auth context

--- src/App.js
+++ src/App.js
@@ -11,4 +11,4 @@ function App() {
 }
 
 export default App;
-// 61 number module er 4 number ta ses and 5 number ta start
+// 61 number module er 5 number ta ses and 6 number ta start
---
+++ src/context/AuthProvider/AuthProvider.js
@@ -0,0 +1,13 @@
+import React, { createContext } from "react";
+
+export const AuthContext = createContext();
+
+const AuthProvider = ({ children }) => {
+  const user = { displayName: "Sazzad Hossain" };
+  const authInfo = { user };
+  return (
+    <AuthContext.Provider value={authInfo}>{children}</AuthContext.Provider>
+  );
+};
+
+export default AuthProvider;
