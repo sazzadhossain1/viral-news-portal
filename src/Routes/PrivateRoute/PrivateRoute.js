7d5732fd8a8fec403e0b30267b48f7d7905fa522
sazzadhossain1 <sazzadtomal1@gmail.com>
2023-04-28 17:36:22 +0600

    set loading spinner and private route reload problem solve

--- src/Routes/PrivateRoute/PrivateRoute.js
+++ src/Routes/PrivateRoute/PrivateRoute.js
@@ -1,10 +1,14 @@
 import React, { useContext } from "react";
 import { AuthContext } from "../../context/AuthProvider/AuthProvider";
 import { Navigate, useLocation } from "react-router-dom";
+import { Spinner } from "react-bootstrap";
 
 const PrivateRoute = ({ children }) => {
-  const { user } = useContext(AuthContext);
+  const { user, loading } = useContext(AuthContext);
   const location = useLocation();
+  if (loading) {
+    return <Spinner animation="grow" />;
+  }
   if (!user) {
     return <Navigate to="/login" state={{ from: location }} replace></Navigate>;
   }
--- src/context/AuthProvider/AuthProvider.js
+++ src/context/AuthProvider/AuthProvider.js
@@ -13,24 +13,29 @@ export const AuthContext = createContext();
 const auth = getAuth(app);
 const AuthProvider = ({ children }) => {
   const [user, setUser] = useState();
+  const [loading, setLoading] = useState(true);
 
   // login with google //
   const googleProviderLogin = (provider) => {
+    setLoading(true);
     return signInWithPopup(auth, provider);
   };
 
   // create user by  registration with email and password //
   const createUser = (email, password) => {
+    setLoading(true);
     return createUserWithEmailAndPassword(auth, email, password);
   };
 
   // Login User with email and password //
   const signInUser = (email, password) => {
+    setLoading(true);
     return signInWithEmailAndPassword(auth, email, password);
   };
 
   // log OUt user //
   const logOut = () => {
+    setLoading(true);
     return signOut(auth);
   };
 
@@ -39,6 +44,7 @@ const AuthProvider = ({ children }) => {
     const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
       console.log("user inside state change", currentUser);
       setUser(currentUser);
+      setLoading(false);
     });
     return () => {
       unsubscribe();
@@ -47,6 +53,7 @@ const AuthProvider = ({ children }) => {
 
   const authInfo = {
     user,
+    loading,
     googleProviderLogin,
     logOut,
     createUser,
